2df2ad1884266013b53b6aef246b44de2beb2de7
Simon Kateregga <spkateregga@gmail.com>
2018-01-11 01:00:26 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+    /*==============================================================
+     wow animation - on scroll
+     ==============================================================*/
+    var wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 0,
+        mobile: false,
+        live: true
+    });
+    wow.init();
